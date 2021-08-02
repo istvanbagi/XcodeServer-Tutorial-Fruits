fb0b5477c0fb47ea6e23204fbcd86f3ee8e64c36
Istvan Bagi <istbagi@yahoo.com>
2021-08-02 18:28:43 +0200

    - Fix.

--- Fruits/Views/ContentView.swift
+++ Fruits/Views/ContentView.swift
@@ -33,28 +33,29 @@
 import SwiftUI
 
 struct FruitRow: View {
-	var fruit: Fruit
+  var fruit: Fruit
 
-	var body: some View {
-		Text(fruit.name)
-	}
+  var body: some View {
+    Text(fruit.name)
+  }
 }
 
 struct ContentView: View {
-	@ObservedObject var viewModel = FruitListViewModel()
+  @ObservedObject var viewModel = FruitListViewModel()
 
-	var body: some View {
-		return
-			NavigationView {
-				List(viewModel.fruits) { fruit in
-					FruitRow(fruit: fruit)
-				}
-				.onAppear {
-					self.viewModel.loadFruits()
-				}
-				.navigationBarTitle("Fruits")
-			}
-	}
+  var body: some View {
+    return
+      NavigationView {
+        List(viewModel.fruits) { fruit in
+          FruitRow(fruit: fruit)
+        }
+        .onAppear {
+          viewModel.loadFruits()
+        }
+        .navigationBarTitle("Fruits")
+      }
+      .navigationViewStyle(StackNavigationViewStyle())
+  }
 }
 
 #if DEBUG
